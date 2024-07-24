2de7362de65caf9646453bf5a327159b282759df
mymom0937 <mymom0937@gmail.com>
2024-07-24 10:59:52 +0100

    initial commit for amazon backend deployement

---
+++ index.js
@@ -0,0 +1,39 @@
+const cors = require("cors");
+const express = require("express");
+const dotenv = require("dotenv");
+const { error } = require("console");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    Message: "Success !",
+  });
+});
+
+app.post("/payment/create", async (req, res) => {
+  const total = req.query.total;
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(403).json({
+      message: "total must be greater than 0",
+    });
+  }
+});
+
+app.listen(5000, (error) => {
+  if (error) throw error
+  console.log("Amazon Server Running on PORT:5000, http://localhost:5000")
+})
